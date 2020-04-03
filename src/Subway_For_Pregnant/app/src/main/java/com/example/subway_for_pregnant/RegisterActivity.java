746f96462c2570cd639e57dcd000ca3ad12221a7
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2020-04-03 12:27:36 +0900

    로그인 및 회원가입 초기 구현

---
+++ src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/RegisterActivity.java
@@ -0,0 +1,108 @@
+package com.example.subway_for_pregnant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "SignUpActivity";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.singUpButton).setOnClickListener(onClickListener);
+    }
+
+   /* @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }*/
+
+    @Override public void onBackPressed(){
+        super.onBackPressed();
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.singUpButton:
+                    signUp();
+                    break;
+            }
+
+        }
+    };
+
+    private void signUp(){
+
+        String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
+        String passwordCheck = ((EditText)findViewById(R.id.passwordcheckEditText)).getText().toString();
+
+        if(email.length() > 0 && password.length()>0 && passwordCheck.length()>0){
+            if(password.equals(passwordCheck)) {
+
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startToast("회원가입에 성공하였습니다.");
+                                    myStartActivity(MainActivity.class);
+                                } else {
+                                    if(task.getException() != null){
+                                        startToast(task.getException().toString());
+                                    }
+
+                                }
+
+                                // ...
+                            }
+                        });
+            }
+            else{
+                startToast("비밀번호가 일치하지 않습니다");
+
+            }
+        }
+        else{
+            startToast("이메일 또는 비밀번호를 입력해 주세요.");
+        }
+    }
+
+    private void startToast(String msg){
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void myStartActivity(Class c){
+        Intent intent=new Intent(this,c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
