cd0b3c4e20eada9b6f0ffddb8156772971040dbb
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2020-04-03 17:41:28 +0900

    로그인 및 회원가입 구현 초기 코드 테스트 완료

--- src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/MainActivity.java
+++ src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.subway_for_pregnant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user == null){
+            myStartActivity(RegisterActivity.class);
+        }else{
+            /*myStartActivity(MemberInitActivity.class);
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference docRef = db.collection("users").document(user.getUid());
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+
+                        if(document != null){
+                            if (document.exists()) {
+
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Log.d(TAG, "No such document");
+                                myStartActivity(MemberInitActivity.class);
+                            }
+                        }
+
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });*/
+            ///
+            /// 멤버 정보 등록 화면
+            /// 미구현
+
+        }
+
+        findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.logoutButton:
+                    FirebaseAuth.getInstance().signOut();
+                    myStartActivity(RegisterActivity.class);
+                    break;
+            }
+        }
+    };
+
+    private void myStartActivity(Class c){
+        Intent intent=new Intent(this,c);
+        startActivity(intent);
     }
 }
