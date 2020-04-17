eee2d785f34fa928039b5629fdfaf6ea50fde983
dh1202kim <dh1202kim@gmail.com>
2020-04-17 16:19:02 +0900

    main 수정

--- src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/MainActivity.java
+++ src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/MainActivity.java
@@ -31,60 +31,26 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         if (user == null) {
             myStartActivity(LoginActivity.class);
         } else {
-            db.collection("user").whereEqualTo("id", user.getEmail()).whereEqualTo("isPregnant", true)
+            db.collection("user").whereEqualTo("id", user.getEmail())//.whereEqualTo("isPregnant", true)
                     .get()
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if (task.isSuccessful()) {
                                 for (QueryDocumentSnapshot document : task.getResult()) {
-                                    Log.d(TAG, document.getId() + " => " + document.getData());
+                                    Log.d(TAG, document.getId() + " => " + document.getData().get("isPregnant"));
                                 }
                             } else {
                                 Log.d(TAG, "Error getting documents: ", task.getException());
-                                myStartActivity(PregnantInitActivity.class);
                             }
                         }
                     });
         }
-        /*
-            if(user == null) {
-                myStartActivity(LoginActivity.class);
-            } else if { // 임산부가 등록되지 않았다면
-            myStartActivity(PregnantInitActivity.class);
-            DocumentReference docRef = db.collection("users").document(user.getUid());
-            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-
-                        if(document != null){
-                            if (document.exists()) {
-
-                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                            } else {
-                                Log.d(TAG, "No such document");
-                                myStartActivity(MemberInitActivity.class);
-                            }
-                        }
-
-                    } else {
-                        Log.d(TAG, "get failed with ", task.getException());
-                    }
-                }
-            });*/
-        ///
-        /// 멤버 정보 등록 화면
-        /// 미구현
-        // 임산부 등록까지 완료된 유저라면
-
-
         findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
+
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
