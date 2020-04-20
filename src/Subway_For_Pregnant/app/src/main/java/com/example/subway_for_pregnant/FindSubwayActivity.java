7da15fd51e02120dc5f128ed4912ecb18ed578f5
JihoonKwak <kkwakjh@naver.com>
2020-04-21 01:25:09 +0900

    노선 간편도 기능 구현 (텍스트)
    노선 간편도 레이아웃에 기능 구현 (텍스트로 출력)

--- src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/FindSubwayActivity.java
+++ src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/FindSubwayActivity.java
@@ -1,20 +1,12 @@
 package com.example.subway_for_pregnant;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import androidx.annotation.IdRes;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.odsay.odsayandroidsdk.API;
@@ -29,11 +21,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.simple.JSONArray;
-import org.json.JSONObject;
 import org.json.JSONException;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class FindSubwayActivity extends AppCompatActivity {
 
@@ -76,31 +64,31 @@ public class FindSubwayActivity extends AppCompatActivity {
         public void onSuccess(ODsayData oDsayData, API api) {
 
             jsonObject = oDsayData.getJson();
-
+            myStartActivity(TrainActivity.class);
+/*
             try {
-                    //tv_data.setText(jsonObject.toString());
-
-                    tv_data.setText(
-                            "출발역 : " + jsonObject.getJSONObject("result").getString("globalStartName")
-                                    + "\n"
-                                    + "도착역 : " + jsonObject.getJSONObject("result").getString("globalEndName")
-                                    + "\n"
-                                    + "걸리는 시간 : " + jsonObject.getJSONObject("result").getInt("globalTravelTime") + "분"
-                                    + "\n"
-                                    + "총 거리: " + jsonObject.getJSONObject("result").getInt("globalDistance") + "km"
-                                    + "\n"
-                                    + "총 정거장 : " + jsonObject.getJSONObject("result").getInt("globalStationCount") + "정거장"
-                                    + "\n"
-                                    + "카드요금 : " + jsonObject.getJSONObject("result").getInt("fare") + "원"
-                                    + "\n"
-                                    + "현금 : " + jsonObject.getJSONObject("result").getInt("cashFare") + "원"
-                    );
-                }
-            catch (JSONException e) {
+                //tv_data.setText(jsonObject.toString());
+                //tv_data.setText("" + );
+                tv_data.setText(
+                    "출발역 : " + jsonObject.getJSONObject("result").getString("globalStartName")
+                        + "\n"
+                        + "도착역 : " + jsonObject.getJSONObject("result").getString("globalEndName")
+                        + "\n"
+                        + "걸리는 시간 : " + jsonObject.getJSONObject("result").getInt("globalTravelTime") + "분"
+                        + "\n"
+                        + "총 거리: " + jsonObject.getJSONObject("result").getInt("globalDistance") + "km"
+                        + "\n"
+                        + "총 정거장 : " + jsonObject.getJSONObject("result").getInt("globalStationCount") + "정거장"
+                        + "\n"
+                        + "카드요금 : " + jsonObject.getJSONObject("result").getInt("fare") + "원"
+                        + "\n"
+                        + "현금 : " + jsonObject.getJSONObject("result").getInt("cashFare") + "원"
+                );
+            } catch (JSONException e) {
+                tv_data.setText("ㅁㄴㅇㄻㄴㅇㄹ");
                 e.printStackTrace();
-            }
-            }
-
+            } */
+        }
 
         @Override
         public void onError(int i, String errorMessage, API api) {
@@ -128,6 +116,59 @@ public class FindSubwayActivity extends AppCompatActivity {
             }
         }
     };
+
+    private void myStartActivity(Class c){
+        Intent intent = new Intent(getApplicationContext(), c);
+
+        try {
+            intent.putExtra("globalStartName", jsonObject.getJSONObject("result").getString("globalStartName"));
+            intent.putExtra("globalEndName", jsonObject.getJSONObject("result").getString("globalEndName"));
+            intent.putExtra("globalTravelTime", jsonObject.getJSONObject("result").getInt("globalTravelTime"));
+            intent.putExtra("globalDistance", jsonObject.getJSONObject("result").getInt("globalDistance"));
+            intent.putExtra("globalStationCount", jsonObject.getJSONObject("result").getInt("globalStationCount"));
+            intent.putExtra("fare", jsonObject.getJSONObject("result").getInt("fare"));
+            intent.putExtra("cashFare", jsonObject.getJSONObject("result").getInt("cashFare"));
+
+            intent.putExtra("driveInfoLength", jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").length());
+            for (int i = 0; i < jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").length(); i++) {
+                intent.putExtra("driveInfoLaneID" + i, jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").getJSONObject(i).getString("laneID"));
+                intent.putExtra("driveInfoLaneName" + i, jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").getJSONObject(i).getString("laneName"));
+                intent.putExtra("driveInfoStartName" + i, jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").getJSONObject(i).getString("startName"));
+                intent.putExtra("driveInfoStationCount" + i, jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").getJSONObject(i).getInt("stationCount"));
+                intent.putExtra("driveInfoWayCode" + i, jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").getJSONObject(i).getInt("wayCode"));
+                intent.putExtra("driveInfoWayName" + i, jsonObject.getJSONObject("result").getJSONObject("driveInfoSet").getJSONArray("driveInfo").getJSONObject(i).getString("wayName"));
+            }
+
+            intent.putExtra("stationsLength", jsonObject.getJSONObject("result").getJSONObject("stationSet").getJSONArray("stations").length());
+            for (int i = 0; i < jsonObject.getJSONObject("result").getJSONObject("stationSet").getJSONArray("stations").length(); i++) {
+                intent.putExtra("stationsStartID" + i, jsonObject.getJSONObject("result").getJSONObject("stationSet").getJSONArray("stations").getJSONObject(i).getInt("startID"));
+                intent.putExtra("stationsStartName" + i, jsonObject.getJSONObject("result").getJSONObject("stationSet").getJSONArray("stations").getJSONObject(i).getString("startName"));
+                intent.putExtra("stationsEndSID" + i, jsonObject.getJSONObject("result").getJSONObject("stationSet").getJSONArray("stations").getJSONObject(i).getInt("endSID"));
+                intent.putExtra("stationsEndName" + i, jsonObject.getJSONObject("result").getJSONObject("stationSet").getJSONArray("stations").getJSONObject(i).getString("endName"));
+                intent.putExtra("stationsTravelTime" + i, jsonObject.getJSONObject("result").getJSONObject("stationSet").getJSONArray("stations").getJSONObject(i).getInt("travelTime"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            intent.putExtra("exChangeInfoLength", jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").length());
+            for (int i = 0; i < jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").length(); i++) {
+                intent.putExtra("exChangeInfoLaneName" + i, jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").getJSONObject(i).getString("laneName"));
+                intent.putExtra("exChangeInfoStartName" + i, jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").getJSONObject(i).getString("startName"));
+                intent.putExtra("exChangeInfoExName" + i, jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").getJSONObject(i).getString("exName"));
+                intent.putExtra("exChangeInfoExSID" + i, jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").getJSONObject(i).getInt("exSID"));
+                intent.putExtra("exChangeInfoFastTrain" + i, jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").getJSONObject(i).getInt("fastTrain"));
+                intent.putExtra("exChangeInfoFastDoor" + i, jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").getJSONObject(i).getInt("fastDoor"));
+                intent.putExtra("exChangeInfoExWalkTime" + i, jsonObject.getJSONObject("result").getJSONObject("exChangeInfoSet").getJSONArray("exChangeInfo").getJSONObject(i).getInt("exWalkTime"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 
     private String findStationCode(String stationName) {
 
