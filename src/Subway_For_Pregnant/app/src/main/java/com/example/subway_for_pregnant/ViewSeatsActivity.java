9dddeb1f22ac9f8e353279b5662fce97006a4e68
JihoonKwak <kkwakjh@naver.com>
2020-04-29 00:58:55 +0900

    좌석 예약 레이아웃 수정
    DB에서 데이터값 받아오기 전 단계까지 구현.

--- src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/ViewSeatsActivity.java
+++ src/Subway_For_Pregnant/app/src/main/java/com/example/subway_for_pregnant/ViewSeatsActivity.java
@@ -1,70 +1,133 @@
 package com.example.subway_for_pregnant;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class ViewSeatsActivity extends AppCompatActivity {
+    int seat1_State = -1, seat2_State = -1;  //0 = 예약가능, 1 = 일반인 사용중, 2 = 예약 불가.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_seats);
 
-        findViewById(R.id.button_State1).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State2).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State3).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State4).setOnClickListener(onClickListener);
+        findViewById(R.id.button_StateLeft).setOnClickListener(onClickListener);
         findViewById(R.id.button_State5).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State6).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State7).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State8).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State9).setOnClickListener(onClickListener);
-        findViewById(R.id.button_State10).setOnClickListener(onClickListener);
+        findViewById(R.id.button_State4).setOnClickListener(onClickListener);
+        findViewById(R.id.button_State3).setOnClickListener(onClickListener);
+        findViewById(R.id.button_State2).setOnClickListener(onClickListener);
+        findViewById(R.id.button_StateRight).setOnClickListener(onClickListener);
+
+        findViewById(R.id.button_Seat1).setOnClickListener(onClickListener);
+        findViewById(R.id.button_Seat2).setOnClickListener(onClickListener);
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()){
-                case R.id.button_State1:
-                    myStartActivity(Ready2Activity.class);
-                    break;
-                    //임시로 지정
-                //나중에 DB에서 좌석 현황 불러올 것
-                case R.id.button_State2:
-                    startToast("이미 예약된 좌석입니다.");
-                    break;
-                case R.id.button_State3:
-                    startToast("이미 예약된 좌석입니다.");
-                    break;
-                case R.id.button_State4:
-                    startToast("이미 예약된 좌석입니다.");
+                case R.id.button_StateLeft:
+                    //나중에 DB에서 좌석 현황 불러올 것.
+                    checkSeats(v);  //불러온 데이터로 좌석 버튼 2개 색상 변경. 현재는 색상 변경 임의로 지정.
                     break;
                 case R.id.button_State5:
-                    startToast("이미 예약된 좌석입니다.");
+                    checkSeats(v);
                     break;
-                case R.id.button_State6:
-                    startToast("이미 예약된 좌석입니다.");
+                case R.id.button_State4:
+                    checkSeats(v);
                     break;
-                case R.id.button_State7:
-                    startToast("이미 예약된 좌석입니다.");
+                case R.id.button_State3:
+                    checkSeats(v);
                     break;
-                case R.id.button_State8:
-                    startToast("이미 예약된 좌석입니다.");
+                case R.id.button_State2:
+                    checkSeats(v);
                     break;
-                case R.id.button_State9:
-                    startToast("이미 예약된 좌석입니다.");
+                case R.id.button_StateRight:
+                    checkSeats(v);
                     break;
-                case R.id.button_State10:
-                    startToast("이미 예약된 좌석입니다.");
+
+                case R.id.button_Seat1:
+                    if (seat1_State == 0) {
+                        myStartActivity(Ready2Activity.class);
+                    }
+                    else if (seat1_State == 1) {
+                        startToast("앉아있는 일반인이 있습니다.");
+                    }
+                    else if (seat1_State == 2) {
+                        startToast("이미 예약되거나 다른 산모가 사용 중인 좌석입니다.");
+                    }
+                    else {
+                        startToast("에러입니다.");
+                    }
+                    break;
+                case R.id.button_Seat2:
+                    if (seat2_State == 0) {
+                        myStartActivity(Ready2Activity.class);
+                    }
+                    else if (seat2_State == 1) {
+                        startToast("앉아있는 일반인이 있습니다.");
+                    }
+                    else if (seat2_State == 2) {
+                        startToast("이미 예약되거나 다른 산모가 사용 중인 좌석입니다.");
+                    }
+                    else {
+                        startToast("에러입니다.");
+                    }
                     break;
             }
         }
     };
+
+    private void checkSeats(View v) {
+        Button btn = (Button) findViewById(v.getId());
+        int emptySeats = Integer.parseInt(btn.getText().toString());
+
+        //임의로 색상 변경 설정. DB에서 데이터를 받으면 여기 if문 조건을 변경해야함.
+        if (emptySeats == 2) {
+            startToast(emptySeats + "개의 좌석이 비어있습니다.");
+            seat1_State = 0;
+            seat2_State = 0;
+            setBtnColor(R.id.button_Seat1, seat1_State);
+            setBtnColor(R.id.button_Seat2, seat2_State);
+        }
+        else if (emptySeats == 1) {
+            startToast(emptySeats + "개의 좌석이 비어있습니다.");
+            seat1_State = 2;
+            seat2_State = 0;
+            setBtnColor(R.id.button_Seat1, seat1_State);
+            setBtnColor(R.id.button_Seat2, seat2_State);
+        }
+        else {
+            startToast("좌석 데이터 오류입니다.");
+            seat1_State = -1;
+            seat2_State = -1;
+            setBtnColor(R.id.button_Seat1, seat1_State);
+            setBtnColor(R.id.button_Seat2, seat2_State);
+        }
+    }
+
+    private void setBtnColor(int btnId, int check) {
+        if (check == 0) {
+            findViewById(btnId).setBackgroundColor(Color.parseColor("#008000")); //Green
+        }
+        else if (check == 1) {
+            findViewById(btnId).setBackgroundColor(Color.parseColor("#FFA500")); //Gold
+        }
+        else if (check == 2) {
+            findViewById(btnId).setBackgroundColor(Color.parseColor("#FF0000")); //Red
+        }
+        else {
+            findViewById(btnId).setBackgroundColor(Color.parseColor("#545454")); //Dark Gray
+        }
+    }
 
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
