9fea52e743f11638cad780008ae2f45cb98ba6e3
SangWon Kim <gini0723@gmail.com>
2020-05-28 16:27:23 +0900

    익스프레스 서버 수정
    로드셀 센서의 값이 테스트 DB의 값이 아닌 실제 앱과 동작하는 DB의 좌석 값을 변경하도록 수정

--- src/node_server/app.js
+++ src/node_server/app.js
@@ -104,10 +104,10 @@ client.on("message", (topic, message) => {
   console.log(obj.seat);
   console.log(data.s1_isSit);
 
-  //db.collection('Demo_subway').doc('line8').collection('Up').doc('2101').
-  //collection('car').doc('1').update(data);
+  db.collection('Demo_subway').doc('line8').collection('Up').doc('2101').
+  collection('car').doc('1').update(data);
 
-  db.collection('test').doc('tnf').update(data);
+  // db.collection('test').doc('tnf').update(data);
 })
 
 function pubMinor(){
