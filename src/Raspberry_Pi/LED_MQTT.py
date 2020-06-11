60f94a47fa86727492c3bf0f774c4a6821f32d1e
SangWon Kim <gini0723@gmail.com>
2020-06-12 00:57:22 +0900

    LED 및 로드셀 동작 수정
    LED NeoPixel 이용 하나의 LED로 RGB 전부 구현, 로드셀은 처음 시작시 DB의 좌석 사용값 자동으로 초기화 하도록 함

--- src/Raspberry_Pi/LED_MQTT.py
+++ src/Raspberry_Pi/LED_MQTT.py
@@ -1,43 +1,63 @@
 #-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+
+from rpi_ws281x import *
+import argparse
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-LED_All, LED_Res = 17, 18
-GPIO.setup(LED_All, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(LED_Res, GPIO.OUT, initial = GPIO.LOW)
+# LED configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
 
 
 def led_pattern(ptn_type):
-    
-    if str(ptn_type) == "1":
-        for i in range(5):
-            GPIO.output(LED_All, GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(LED_All, GPIO.LOW)
-            time.sleep(0.5)
-        GPIO.output(LED_All, GPIO.HIGH)
-        
-    elif str(ptn_type) == "2":
-        for i in range(5):
-            GPIO.output(LED_Res, GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(LED_Res, GPIO.LOW)
-            time.sleep(0.5)
-        GPIO.output(LED_Res, GPIO.HIGH)
-        
-    elif str(ptn_type) == "0":
-        GPIO.output(LED_All, GPIO.LOW)
-        GPIO.output(LED_Res, GPIO.LOW)
 
+    if str(ptn_type) == "b'1'":
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(255, 165, 0))
+        strip.show()
+        time.sleep(0.25)
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(0,0,0))
+        strip.show()
+        time.sleep(0.25)
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(255, 165, 0))
+        strip.show()
+
+    elif str(ptn_type) == "b'2'":
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(255, 0, 0))
+        strip.show()
+        time.sleep(0.25)
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(0,0,0))
+        strip.show()
+        time.sleep(0.25)
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(255, 0, 0))
+        strip.show()
+
+    elif str(ptn_type) == "b'0'":
+        for i in range(3):
+            for j in range(strip.numPixels()):
+                strip.setPixelColor(j, Color(0,0,0))
+            strip.show()
 
 # 서버로부터 CONNTACK 응답을 받을 때 호출되는 콜백
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    #client.subscribe("$SYS/#")
-    client.subscribe("LED601") #구독 "LED2"
+    client.subscribe("LED601") #구독 "LED601"
 
 # 서버로부터 publish message를 받을 때 호출되는 콜백
 def on_message(client, userdata, msg):
@@ -45,11 +65,15 @@ def on_message(client, userdata, msg):
     print(str(msg.payload))
     led_pattern(msg.payload)
 
-    
-client = mqtt.Client() #client 오브젝트 생성
-client.on_connect = on_connect #콜백설정
-client.on_message = on_message #콜백설정
 
-client.connect("192.168.137.1", 1883) 
-#라즈베리파이3 MQTT 브로커에 연결
-client.loop_forever()
+if __name__ == '__main__':
+    # Create NeoPixel object with appropriate configuration.
+    # Intialize the library (must be called once before other functions).
+
+    client = mqtt.Client() #client 오브젝트 생성
+    client.on_connect = on_connect #콜백설정
+    client.on_message = on_message #콜백설정
+
+    client.connect("192.168.137.1", 1883)
+    #라즈베리파이3 MQTT 브로커에 연결
+    client.loop_forever()
--- src/Raspberry_Pi/Load_Cell.py
+++ src/Raspberry_Pi/Load_Cell.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt
 
 EMULATE_HX711=False
 
-referenceUnit = 22168 
+referenceUnit = 21500 # 21500
 
 if not EMULATE_HX711:
     import RPi.GPIO as GPIO
@@ -32,11 +32,13 @@ mqtt = mqtt.Client("Pi1") # MQTT Client Name
 mqtt.connect("192.168.137.1", 1883) # MQTT Broker Add
 
 isSit = "false"
+mqtt.publish("Pi1", "{\"s1_isSit\":false}") # initialization
 
 while True:
     try:
         val = hx.get_weight(5)
         print(val)
+        
         if val > 20: # load over 20Kg
             if isSit != "true":
                 mqtt.publish("Pi1", "{\"s1_isSit\":true}")
@@ -45,7 +47,7 @@ while True:
             if isSit != "false":
                 mqtt.publish("Pi1", "{\"s1_isSit\":false}")
                 isSit = "false"
-
+        
         hx.power_down()
         hx.power_up()
         time.sleep(1)
